d2d8617ac67acee03c171e735731ac162e1fb9ee
Avishek Saha <55613672+avi1023@users.noreply.github.com>
2020-02-13 10:53:37 +0600

    Update 1.php

--- 1.php
+++ 1.php
@@ -10,7 +10,7 @@
 
 	<?php
 // define variables and set to empty values
-$NameErr = $EmailErr = $genderErr = $DegreeErr = $bgErr = "";
+$NameErr = $EmailErr = $genderErr = $bdayErr = $DegreeErr = $bgErr = "";
 $Name = $Email = $bday =$gender = $Degree = $bg = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -34,26 +34,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
     
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
+  
+  if (empty($_POST["bday"])) {
+    $bdayErr = "Birthday is required";
   } else {
-    $comment = test_input($_POST["comment"]);
+    $bday = test_input($_POST["bday"]);
   }
 
   if (empty($_POST["gender"])) {
     $genderErr = "Gender is required";
   } else {
     $gender = test_input($_POST["gender"]);
+  }
+  
+  if (empty($_POST["Degree"])) {
+    $DegreeErr = "Degree is required";
+  } else {
+    $Degree = test_input($_POST["Degree"]);
+  }
+  
+  if (empty($_POST["bg"])) {
+    $bgErr = "Blood Group is required";
+  } else {
+    $Degree = test_input($_POST["bg"]);
   }
 }
 
@@ -68,27 +72,36 @@ function test_input($data) {
 
 
 	<form method="post" action="<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]); ?>" >
-		Name <input type="text" name="Name" required><br>
-		Email <input type="text" name="Email" required><br>
-		Date Of Birth <input type="date" name="bday"><br>
-		Gender  <input type="radio" name="gender" value="male">Male
-  			    <input type="radio" name="gender" value="female">Female
-  			    <input type="radio" name="gender" value="other">Other <br>
-  		Degree  <input type="checkbox" name="Degree" value="SSC">SSC 
-  				<input type="checkbox" name="Degree" value="HSC">HSC 
-  				<input type="checkbox" name="Degree" value="BSc">BSc
-  				<input type="checkbox" name="Degree" value="MSc">MSc <br>
+		Name <input type="text" name="Name" value="<?php echo $Name;?>">
+  <span class="error">* <?php echo $NameErr;?></span> <br>
+		Email <input type="text" name="Email" value="<?php echo $Email;?>">
+  <span class="error">* <?php echo $EmailErr;?></span><br>
+		Date Of Birth <input type="date" name="bday" value="<?php echo $bday;?>">
+  <span class="error">* <?php echo $bdayErr;?></span><br>
+		Gender  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+  			    <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+  			    <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other 
+				<span class="error">* <?php echo $genderErr;?></span>
+				<br>
+  		Degree  <input type="checkbox" name="Degree" <?php if (isset($Degree) && $Degree=="SSC") echo "checked";?> value="SSC">SSC 
+  				<input type="checkbox" name="Degree" <?php if (isset($Degree) && $Degree=="HSC") echo "checked";?> value="HSC">HSC 
+  				<input type="checkbox" name="Degree" <?php if (isset($Degree) && $Degree=="BSc") echo "checked";?> value="BSc">BSc
+  				<input type="checkbox" name="Degree" <?php if (isset($Degree) && $Degree=="MSc") echo "checked";?> value="MSc">MSc 
+				<span class="error">* <?php echo $DegreeErr;?></span>
+				<br>
   		Blood Group <br>
 			    <select name="bg">
-			    <option value="A+">A+</option>
-				<option value="A-">A-</option>
-				<option value="AB+">AB+</option>
-				<option value="AB-">AB-</option>
-				<option value="B+">B+</option>
-				<option value="B-">B-</option>
-				<option value="O+">O+</option>
-				<option value="O-">O-</option>
-				</select> <br><br>
+			    <option <?php if (isset($bg) && $bg=="A+") echo "selected";?> value="A+">A+</option>
+				<option <?php if (isset($bg) && $bg=="A-") echo "selected";?> value="A-">A-</option>
+				<option <?php if (isset($bg) && $bg=="AB+") echo "selected";?> value="AB+">AB+</option>
+				<option <?php if (isset($bg) && $bg=="AB-") echo "selected";?> value="AB-">AB-</option>
+				<option <?php if (isset($bg) && $bg=="B+") echo "selected";?> value="B+">B+</option>
+				<option <?php if (isset($bg) && $bg=="B-") echo "selected";?> value="B-">B-</option>
+				<option <?php if (isset($bg) && $bg=="O+") echo "selected";?> value="O+">O+</option>
+				<option <?php if (isset($bg) && $bg=="O-") echo "selected";?> value="O-">O-</option>
+				</select> 
+				<span class="error">* <?php echo $bgErr;?></span>
+				<br><br>
 		<input type="submit" value="Submit"> <br>
 	</form>
 
